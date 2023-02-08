24ad9bbe1ad88167da1f30df4fe50b0c91f0f0af
Nick777-Pixel <81410494+Nick777-Pixel@users.noreply.github.com>
2023-02-08 01:51:32 -0600

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,7 +14,7 @@ function Header() {
           className='button--primary w-auto text-sm mb-3 flex items-center gap-2'
           onClick={console.log}
         >
-          <GithubIcon />
+          <faTwitter />
           Star on GitHub
         </button>
       </a>
